2755b3606a892c4dff7427fcef0a058ad692e18e
york <york0704@gmail.com>
2022-09-10 02:13:13 +0800

    add nav

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,17 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+$(document).ready(function () {
+  $('.fa-search-sm').click(
+    function (event) {
+      $('.navOrigin').addClass('d-none');
+      $('.searchHolder').removeClass('d-none');
+    });
+
+  $('.fa-angle-left-sm').click(
+    function (event) {
+      $('.searchHolder').addClass('d-none');
+      $('.navOrigin').removeClass('d-none');
+    });
+});
