633dfe3278c11fff3f145fe31daa4062c1c95b79
york <york0704@gmail.com>
2022-09-14 22:36:50 +0800

    finish lv2

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -15,3 +15,33 @@ $(document).ready(function () {
       $('.navOrigin').removeClass('d-none');
     });
 });
+
+var swiper = new Swiper(".iphoneSwiper", {
+  loop: true,
+  speed: 500,
+  // autoplay: {
+  //   delay: 3000,
+  
+  // },
+  disableOnInteraction: false,
+  centeredSlides: true,
+  slidesPerView: 1,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.3
+    },
+    1200: {
+      slidesPerView: 2.88
+    },
+    1400: {
+      slidesPerView: 2.9
+    }
+  },
+  spaceBetween: 24,
+  effect: 'slide',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  grabCursor: true,
+});
